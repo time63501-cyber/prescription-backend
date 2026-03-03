894a25ed38b90f70f42f64ac0a20395211e62aa8
time63501-cyber <time63501@gmail.com>
2026-03-03 08:31:20 +0000

    small error-quotes

--- debugging-gemini-api.py
+++ debugging-gemini-api.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 import os
 
-genai.configure(api_key="os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 for model in genai.list_models():
     if "generateContent" in model.supported_generation_methods:
