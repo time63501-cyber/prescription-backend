4c2098792354e0ed4e7dd5fcf9f85b420801836a
time63501-cyber <time63501@gmail.com>
2026-03-03 08:33:23 +0000

    quotes-error

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret_key")
-    GEMINI_API_KEY = "os.getenv("GEMINI_API_KEY")"
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
     UPLOAD_FOLDER = "uploads"
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max upload
